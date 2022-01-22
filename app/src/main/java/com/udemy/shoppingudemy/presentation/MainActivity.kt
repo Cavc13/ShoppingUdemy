b8a8d6f49e62e44feeeb03a47fcbb973d101434f
Cavc13 <13liver@mail.ru>
2022-01-22 18:11:44 +0300

    Create domain

--- app/src/main/java/com/udemy/shoppingudemy/presentation/MainActivity.kt
+++ app/src/main/java/com/udemy/shoppingudemy/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.udemy.shoppingudemy
+package com.udemy.shoppingudemy.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.udemy.shoppingudemy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
