8cce4c245e1e81cb332a15e96801c88c55453c65
Cavc13 <13liver@mail.ru>
2022-01-25 15:18:16 +0300

    Create second activity

---
+++ app/src/main/java/com/udemy/shoppingudemy/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.udemy.shoppingudemy.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udemy.shoppingudemy.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
