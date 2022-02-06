844707d5204020dda2562c50d5f57bdc7568575a
Cavc13 <13liver@mail.ru>
2022-02-06 20:07:40 +0300

    Update data, repository

---
+++ app/src/main/java/com/udemy/shoppingudemy/data/ShopListMapper.kt
@@ -0,0 +1,22 @@
+package com.udemy.shoppingudemy.data
+
+import com.udemy.shoppingudemy.domain.ShopItem
+
+class ShopListMapper {
+    fun mapEntityToDbModel(shopItem: ShopItem) = ShopItemDbModel(
+            id = shopItem.id,
+            name = shopItem.name,
+            count = shopItem.count,
+            enabled = shopItem.enabled)
+
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel) = ShopItem(
+            id = shopItemDbModel.id,
+            name = shopItemDbModel.name,
+            count = shopItemDbModel.count,
+            enabled = shopItemDbModel.enabled)
+
+
+    fun mapListDbModelToListEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDbModelToEntity(it)
+    }
+}
