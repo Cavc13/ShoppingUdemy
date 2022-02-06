f29dd4daf4945093c8bb5ba2ad61c488e45fc082
Cavc13 <13liver@mail.ru>
2022-02-06 17:37:46 +0300

    databinding

--- app/src/main/java/com/udemy/shoppingudemy/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/udemy/shoppingudemy/presentation/ShopItemViewHolder.kt
@@ -2,10 +2,11 @@ package com.udemy.shoppingudemy.presentation
 
 import android.view.View
 import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.udemy.shoppingudemy.R
+import com.udemy.shoppingudemy.databinding.ItemShopDisabledBinding
 
-class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-    val tvName = view.findViewById<TextView>(R.id.tv_name)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-}
+class ShopItemViewHolder(
+    val binding: ViewDataBinding
+    ): RecyclerView.ViewHolder(binding.root)
